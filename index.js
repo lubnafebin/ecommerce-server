d0987f16894efc87694148f86012cab000990b07
Lubna Febin <lubnafebin13@gmail.com>
2025-09-25 22:37:48 +0530

    added middlewares

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+import cookieParser from "cookie-parser";
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const allowedOrigins = ["http://localhost:5173"];
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.get("/", (req, res) => res.send("working"));
 
