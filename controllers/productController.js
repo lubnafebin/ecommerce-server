a78aeb64dc274888a1ccdeb5a0f9b16f7d3d6431
Lubna Febin <lubnafebin13@gmail.com>
2025-09-26 14:35:25 +0530

    added product controller and model

---
+++ controllers/productController.js
@@ -0,0 +1,11 @@
+//Add product
+export const addProduct = async (req, res) => {};
+
+//get All Product
+export const productList = async (req, res) => {};
+
+//get single product
+export const productById = async (req, res) => {};
+
+//change product in stock
+export const changeStock = async (req, res) => {};
