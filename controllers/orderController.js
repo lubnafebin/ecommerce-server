d76dded6b4f0a0c5a57cac7940f573df134eb692
Lubna Febin <lubnafebin13@gmail.com>
2025-09-30 21:15:55 +0530

    added controller for online payment using stripe

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,5 +1,6 @@
 import Order from "../models/Order.js";
 import Product from "../models/Product.js";
+import stripe from "stripe";
 
 //place order COD
 export const placedOrderCOD = async (req, res) => {
@@ -27,6 +28,74 @@ export const placedOrderCOD = async (req, res) => {
     });
 
     return res.json({ success: true, message: "Order Placed Successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//place order stripe online payment
+export const placedOrderStripe = async (req, res) => {
+  try {
+    const { userId, address, items } = req.body;
+    const { origin } = req.headers;
+    if (!address || items.length === 0) {
+      return res.json({ success: false, message: "Invalid data" });
+    }
+
+    let productData = [];
+
+    //calculate amount using items
+    let amount = await items.reduce(async (acc, item) => {
+      const product = await Product.findById(item.product);
+      productData.push({
+        name: product.name,
+        price: product.offerPrice,
+        quantity: item.quantity,
+      });
+      return (await acc) + product.offerPrice * item.quantity;
+    }, 0);
+
+    //tax charge
+    amount += Math.floor(amount * 0.02);
+
+    const order = await Order.create({
+      userId,
+      items,
+      amount,
+      address,
+      paymentType: "Online",
+    });
+
+    //creating stripe instance
+    const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
+    //creating line items for stripe
+    const line_items = productData.map((item) => {
+      return {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: item.name,
+          },
+          unit_amount: Math.floor(item.price + item.price * 0.2) * 100,
+        },
+        quantity: item.quantity,
+      };
+    });
+
+    //session
+    const session = await stripeInstance.checkout.sessions.create({
+      line_items,
+      mode: "payment",
+      success_url: `${origin}/loader?next=my-orders`,
+      cancel_url: `${origin}/cart`,
+      metadata: {
+        orderId: order._id.toString(),
+        userId,
+      },
+    });
+
+    return res.json({ success: true, url: session.url });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
