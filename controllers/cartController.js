0cc1344732981c4250007710a2b8b769d382d7be
Lubna Febin <lubnafebin13@gmail.com>
2025-10-08 15:32:36 +0530

    added remove item from cart functionality

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -11,3 +11,37 @@ export const updateCart = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+//remove item from cart
+export const removeFromCart = async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    const productId = req.params.id;
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    // Filter out the product to remove it
+    user.cartItems = user.cartItems.filter(
+      (item) => item._id.toString() !== productId.toString()
+    );
+
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Item removed from cart",
+      cartItems: user.cartItems,
+    });
+  } catch (error) {
+    console.error("Error removing item from cart:", error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to remove item from cart",
+    });
+  }
+};
--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
 import authUser from "../middlewares/authUser.js";
-import { updateCart } from "../controllers/cartController.js";
+import { removeFromCart, updateCart } from "../controllers/cartController.js";
 
 const cartRouter = express.Router();
 
 cartRouter.post("/update", authUser, updateCart);
+cartRouter.delete("/remove/:id", authUser, removeFromCart);
 
 export default cartRouter;
